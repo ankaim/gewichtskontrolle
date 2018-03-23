11440a4b8c476c1822c7fb8e3b5d1b73258ec353
anatoly <kaim@mail.ru>
2018-03-23 10:33:24 +0100

    Initial commit

---
+++ src/main/java/de/home/gewichtskontrolle/GewichtskontrolleApplication.java
@@ -0,0 +1,12 @@
+package de.home.gewichtskontrolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GewichtskontrolleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GewichtskontrolleApplication.class, args);
+	}
+}
